dec8621957e2937da373616ca535ea7948e1898d
MUEID MURSALIN RIFAT <catx224@gmail.com>
2026-07-06 21:09:13 +0600

    Remove unused aliases from autodl configuration

--- autodl.js
+++ autodl.js
@@ -5,7 +5,7 @@ const path = require("path");
 module.exports = {
   config: {
     name: "autodl",
-    aliases: ["dl", "download", "saver", "get"],
+    aliases: ["dl", "download"],
     version: "1.0",
     author: "Mueid Mursalin Rifat",
     countDown: 5,
